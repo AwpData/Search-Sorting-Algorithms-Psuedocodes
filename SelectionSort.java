fd998bbf04687e8e8cb0652b39cca03fcc05f383
AwpData <awpdata@gmail.com>
2019-03-13 12:38:41 -0700

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,7 @@
+		Scanner input = new Scanner([INSERT SOURCE]);
+		while (input.hasNextLine()) {
+			String s = input.nextLine();
+			System.out.println(s);
+			if (num.equals([SOLUTION])) {
+				System.out.println("Found!");
+			}
